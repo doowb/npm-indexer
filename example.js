4df53469a8de2a548ae7dabdd51e5e7a1c5f4d32
doowb <brian.woodward@gmail.com>
2017-04-05 16:00:29 -0400

    add code comments for documentation

--- example.js
+++ example.js
@@ -2,9 +2,14 @@
 
 var extend = require('extend-shallow');
 var algolia = require('search-indexer-algolia');
-var npm = require('./');
+var npmIndexer = require('./');
+
+/**
+ * Simple indexer that just transforms and writes information to console.log
+ */
 
 var indexer = {
+  // transform `file` into the `package.json` object from npm and remove `.versions` for easier reading
   collect: function(file, next) {
     var pkg = extend({}, file.pkg);
     pkg.objectID = file.key;
@@ -13,11 +18,18 @@ var indexer = {
     console.log('collect', pkg.name);
     next(null, pkg);
   },
+  // log out `files` to show that `index` is called multiple times with only current files
   index: function(files, options, cb) {
     console.log('index', files);
     cb();
   }
 };
+
+/**
+ * Setup options using the simple indexer and starting at a specific sequence number and only looking at 20 files.
+ * The `keywords` will attempt to match keywords in the files, and only index matching keywords.
+ * Add `includeName: true` to also attempt matching against the package name.
+ */
 
 var options = {
   indexer: indexer,
@@ -26,11 +38,15 @@ var options = {
   keywords: ['chrome']
 };
 
-npm(options)
+/**
+ * Start the stream. The indexer will be called when keywords match. Only matching files will come through the stream.
+ */
+
+npmIndexer(options)
   .on('data', function(file) {
-    // console.log('npm file', file);
+    // console.log('npmIndexer file', file);
   })
   .on('end', function() {
-    console.log('npm end');
+    console.log('npmIndexer end');
   });
 
--- index.js
+++ index.js
@@ -13,9 +13,32 @@ var defaults = {
   db: 'https://skimdb.npmjs.com/registry'
 };
 
-module.exports = function(config) {
-  config = extend({}, defaults, config);
-  var indexer = config.indexer;
+/**
+ * Create a stream of matching npm packages. Use the provided `indexer` to collect and index
+ * the matching npm packages to create search indexes.
+ *
+ * ```js
+ * npmIndexer({
+ *   indexer: require('search-indexer-algolia')(algoliaOptions),
+ *   keywords: ['assembleplugin']
+ * })
+ * .on('data', console.log)
+ * .on('end', console.log.bind(console, 'done'));
+ * ```
+ * @param  {Object} `options` Options specifying the indexer to use and additional options for the [changes stream](https://github.com/doowb/vinyl-changes-stream).
+ * @param  {Object} `options.indexer` Specify the indexer to use for creating a search index. Example: [search-indexer-algolia][].
+ * @param  {Boolean} `options.includeName` Include the package name when determining if the package should be indexed. Defaults to `false`.
+ * @param  {Array} `options.keywords` Array of keywords to use to determine if the package should be indexed. These are matched against the package keywords and the package name if `includeName` is `true`.
+ * @param  {Number} `options.since` Sequence number passed to changes stream that will indicate which sequence number to start with.
+ * @param  {Number} `options.limit` Limit the number of packages returned from the changes stream. Defaults to -1 which will be continuous.
+ * @param  {String} `options.db` CouchDB URI to pull the changes stream from. Defaults to the NPM registry.
+ * @return {Stream} Returns a stream that will have matching packages.
+ * @api public
+ */
+
+module.exports = function npmIndexer(options) {
+  var opts = extend({}, defaults, options);
+  var indexer = opts.indexer;
   if (!indexer) {
     throw new Error('expected a search indexer to be specified');
   }
@@ -25,7 +48,7 @@ module.exports = function(config) {
     clear: true
   });
 
-  var keywords = arrayify(config.keywords);
+  var keywords = arrayify(opts.keywords);
   var matchers = keywords.map(function(keyword) {
     return function(str) {
       return micromatch.contains(str, keyword);
@@ -39,10 +62,9 @@ module.exports = function(config) {
     return koalas.apply(koalas, fns);
   });
 
-  return changes(config)
-    .on('error', console.error)
+  return changes(opts)
     .pipe(through.obj(function(file, enc, next) {
-      var haystack = arrayify(config.includeName ? file.id : null)
+      var haystack = arrayify(opts.includeName ? file.id : null)
         .concat(arrayify(file.json.doc.keywords));
 
       var searches = haystack.map(function(str) {
@@ -55,15 +77,10 @@ module.exports = function(config) {
       next();
     }))
     .pipe(sarge.collect())
-    .on('error', console.error)
-    // pull the data through so the stream doesn't stop
     .on('data', function() {
       sarge.index(function(err) {
         if (err) console.error(err);
       })
-    })
-    .on('end', function() {
-      console.log('done');
     });
 };
 
